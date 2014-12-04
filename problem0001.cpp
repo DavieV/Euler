9e22fbeae4a1c625f6ddbdb58d6d13cd8a6b0654
David Valleau <davidvalleau@gmail.com>
2014-12-04 00:40:56 -0500

    cleaning up older solutions, improved to O(1) complexity

--- problem0001.cpp
+++ problem0001.cpp
@@ -2,12 +2,16 @@
 
 #include <iostream>
 
-using namespace std;
+int sum(int n);
 
-int main(void){
-	int sum = 0;
-	for(int i = 0; i < 1000; i++){
-		if(i % 3 == 0 || i % 5 == 0) sum += i;
-	}
-	cout << sum << endl;
+int main() {
+    std::cout << sum(1000) << std::endl;
 }
+
+int sum(int n) {
+    int threes = (n-1) / 3 * ((n-1) / 3 + 1) / 2 * 3;
+    int fives = (n-1) / 5 * ((n-1) / 5 + 1) / 2 * 5;
+    int fifteens = (n-1) / 15 * ((n-1) / 15 + 1) / 2 * 15;
+    return threes + fives - fifteens;
+}
+
