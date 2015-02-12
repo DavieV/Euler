d5eb59cb739cf003e02e0db5e63b6a3bcdfefa68
David Valleau <davidvalleau@gmail.com>
2015-02-12 12:42:49 -0500

    Optimized one of my old solutions

--- problem0004.cpp
+++ problem0004.cpp
@@ -2,26 +2,25 @@
 * Finds the largest palindrome made by the product of two three-digit numbers
 */
 #include <iostream>
+#include "eutility.hpp"
 
 using namespace std;
 
-bool isPalindrome(string n);
+bool isPalindrome(int n);
 
-int main(void){
-	int max = 0;
-	int prod;
-	for(int i = 100; i < 1000; i++){
-		for(int j = 100; j < 1000; j++){
-			prod = i * j;
-			if(isPalindrome(to_string(prod)) && prod > max) max = prod;
-		}
-	}
-	cout << max << endl;
+int main() {
+    int max = 0;
+    int prod;
+    for (int i = 100; i < 1000; i++) {
+        for (int j = i; j < 1000; j++) {
+            prod = i * j;
+            if (isPalindrome(prod) && prod > max)
+                max = prod;
+        }
+    }
+    cout << max << endl;
 }
 
-bool isPalindrome(string n){
-	for(int i = 0; i < n.length() / 2; i++){
-		if(n[i] != n[n.length() - i - 1]) return false;
-	}
-	return true;
+bool isPalindrome(int n) {
+    return n == reverse_num(n);
 }
